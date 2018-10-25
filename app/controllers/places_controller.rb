49aca78a250907b9198ee77b182aad1f1ffb4c36
gpaolarojas <gpaolarojas@gmail.com>
2018-10-25 09:53:51 -0500

    update3

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,29 +1,35 @@
 class PlacesController < ApplicationController
-    def create
-         @place = Place.create(place_params)
-        #  @place = Place.create({})
-         render json: @place, status: :created
-    end
+  def create
+    @place = Place.create(place_params)
+    #  @place = Place.create({})
+    render json: @place, status: :created
+  end
 
-    def index
-        places = Place.all
-        render json: places, status: :ok
-    end
+  def index
+    places = Place.all
+    render json: places, status: :ok
+  end
 
-    def show
-        @place = Place.find(params[:id])
-        render json: @place, status: :ok
-    end
+  def show
+    @place = Place.find(params[:id])
+    render json: @place, status: :ok
+  end
 
-    def update
-        place= Place.find(params[:id])
-        place.update(place_params)
-        render json: place, status: :ok
-    end
-    
+  def event_places
+    place = Place.find(params[:id])
+    event_places = place.events
+    render json: event_places, status: :ok
+  end
 
-    private 
-    def place_params
-        params.permit(:name, :latitude, :longitude, :address)
-    end
+  def update
+    place = Place.find(params[:id])
+    place.update(place_params)
+    render json: place, status: :ok
+  end
+
+  private
+
+  def place_params
+    params.permit(:name, :latitude, :longitude, :address)
+  end
 end
