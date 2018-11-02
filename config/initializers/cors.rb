0b16868991b274d8a54def51739749f74e809767
Diego Cuervo G <31864640+DIEGOFC6@users.noreply.github.com>
2018-11-02 10:02:27 -0500

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-# config/initializers/cors.rb
-=======
->>>>>>> 54f82f633f2f61f369f6909ede5f424e7cda00cf
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins '*'
@@ -10,7 +7,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         methods: %i(get post put patch delete options head)
     end
   end
-<<<<<<< HEAD
-=======
-  
->>>>>>> 54f82f633f2f61f369f6909ede5f424e7cda00cf
+
