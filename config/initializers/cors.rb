0108ab6a91aeb6cca7eae7b102aff7127a7caabd
gpaolarojas <gpaolarojas@gmail.com>
2018-11-13 16:53:45 -0500

    Usando postgres

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-# config/initializers/cors.rb
-=======
->>>>>>> 54f82f633f2f61f369f6909ede5f424e7cda00cf
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
-      resource '*',
-        headers: :any,
-        methods: %i(get post put patch delete options head)
-    end
+  allow do
+    origins '*'
+    resource '*',
+      headers: :any,
+      methods: %i(get post put patch delete options head)
   end
-<<<<<<< HEAD
-=======
-  
->>>>>>> 54f82f633f2f61f369f6909ede5f424e7cda00cf
+end
