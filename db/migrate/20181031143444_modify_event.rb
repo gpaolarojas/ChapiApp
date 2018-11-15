a1a47e0e303f6ce11ac8a14328ee4e3bfed89cf8
gpaolarojas <gpaolarojas@gmail.com>
2018-11-15 12:59:30 -0500

    mediodia

--- db/migrate/20181031143444_modify_event.rb
+++ db/migrate/20181031143444_modify_event.rb
@@ -1,6 +1,6 @@
 class CreateTables < ActiveRecord::Migration[5.1]
   def change
-    change_column :events, :date, :datetime
+    change_column: :events, :date, :datetime
     add_column:   :events,  :description_event, :string
     add_column:   :events,  :image, :string 
     
